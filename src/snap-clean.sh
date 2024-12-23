2f0b237d39c1ccce7e2af504b4829ea0fb40f04f
Rafael Neri <460273+rafael-neri@users.noreply.github.com>
2024-12-23 11:55:28 -0300

    Update snap-clean.sh

--- src/snap-clean.sh
+++ src/snap-clean.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
-snap list --all | awk '/disabled/{print $1, $3}' |
-    while read snapname revision; do
-        sudo snap remove "$snapname" --revision="$revision"
+# Function to log messages
+log() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - $1"
+}
+
+# Function to remove disabled snap revisions
+remove_disabled_snaps() {
+    log "Starting to remove disabled snap revisions"
+    snap list --all | awk '/disabled/{print $1, $3}' |
+    while read -r snapname revision; do
+        log "Removing $snapname revision $revision"
+        sudo snap remove "$snapname" --revision="$revision" || {
+            log "Failed to remove $snapname revision $revision"
+            exit 1
+        }
     done
+    log "Finished removing disabled snap revisions"
+}
+
+# Main script execution
+remove_disabled_snaps
